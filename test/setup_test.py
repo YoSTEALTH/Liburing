7b55ac7d13c6af937fb7a9e67dca0db9528c863a
YoSTEALTH <ritesh@stealthcentral.com>
2019-12-24 15:40:19 -0600

    `IORING_SETUP_SQPOLL` requires "root" not `io_uring_queue_init()`

--- test/setup_test.py
+++ test/setup_test.py
@@ -15,7 +15,7 @@ def test_setup_polling_io():
     liburing.io_uring_queue_exit(ring)
 
 
-@pytest.mark.skipif(os.getuid() != 0, reason='`io_uring_queue_init()` must be run as "root" user.')
+@pytest.mark.skipif(os.getuid() != 0, reason='`IORING_SETUP_SQPOLL` must be run as "root" user.')
 def test_setup_kernel_side_polling():
     ring = liburing.io_uring()
     assert liburing.io_uring_queue_init(1, ring, liburing.IORING_SETUP_SQPOLL) == 0
