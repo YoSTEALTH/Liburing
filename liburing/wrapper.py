7228566f247acd7ba4b617a1ef2a5e5b5bdfd313
YoSTEALTH <ritesh@stealthcentral.com>
2019-12-24 15:29:31 -0600

    `rewrap` parameter added

--- liburing/wrapper.py
+++ liburing/wrapper.py
@@ -8,25 +8,26 @@ lib = ctypes.cdll.LoadLibrary('liburing.so')
 # lib = ctypes.cdll.LoadLibrary('/opt/liburing/0.3/lib/liburing.so')  # custom liburing path
 
 
-def cwrap(restype, *argtypes, error_check=None):
-    """ Ctypes function wrapper - easy to create, visually readable code & meaningful `help()` docs
+def cwrap(restype, *argtypes, error_check=None, rewrap=None):
+    ''' Ctypes function wrapper - easy to create, visually readable code & meaningful `help()` docs
 
         Type
             restype:        any                 # ctypes return type
             argtypes:       Sequence[any]       # ctypes argument type
             error_check:    bool
+            rewrap:         bool
             return:         lib_fun             # ctypes library function
 
         Example
             >>> @cwrap(ctypes.c_int, ctypes.c_uint, ctypes.c_uint, ctypes.c_uint)
             >>> def io_uring_queue_init(entries, ring, flags):
-            ...     '''
+            ...     """
             ...         Type
             ...             entries:    int
             ...             ring:       io_uring
             ...             flags:      int
             ...             return:     int
-            ...     '''
+            ...     """
 
             # or
 
@@ -34,9 +35,13 @@ def cwrap(restype, *argtypes, error_check=None):
             >>> def io_uring_queue_init(entries: int, ring: io_uring, flags: int) -> int:
             ...     pass
 
+            TODO:
+                - write example for `error_check` and `rewrap`
+
         Note
             - `function_name` created must be same as `lib.function_name`
-    """
+            - `error_check` raises appropriate exception for -errno values
+    '''
     def decorate(func):
         lib_fun = getattr(lib, func.__name__)
         lib_fun.restype = restype
@@ -44,7 +49,7 @@ def cwrap(restype, *argtypes, error_check=None):
 
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            no = lib_fun(*args, **kwargs)
+            no = lib_fun(*args, **kwargs) if not rewrap else lib_fun(*func(*args, **kwargs))
             if error_check and no < 0:  # error
                 raise OSError(-no, os.strerror(-no))
             return no  # success or value
