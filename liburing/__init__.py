c8d0d7ec1e2637461cd81234364fcccd9a5ab178
YoSTEALTH <ritesh@stealthcentral.com>
2019-12-27 13:54:45 -0600

    version 0.0.8

--- liburing/__init__.py
+++ liburing/__init__.py
@@ -5,4 +5,4 @@ from .io_uring import *  # noqa
 
 # enables `help(liburing)` to display everything
 __all__ = [i for i in locals().keys() if not i.startswith(('_', 'cwrap'))]
-__version__ = '0.0.7'
+__version__ = '0.0.8'
