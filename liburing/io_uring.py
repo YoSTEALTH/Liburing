b8fcbeaf6d7eb6027aee9825f087d74f09d20bc0
YoSTEALTH <ritesh@stealthcentral.com>
2019-12-28 19:21:25 -0600

    _fields_ typo

--- liburing/io_uring.py
+++ liburing/io_uring.py
@@ -91,12 +91,12 @@ globals().update(OP.__members__)
 # Internally used
 # ---------------
 class _union_one(ctypes.Union):
-    _fields_ = [('off',   ctypes.c_uint64),  # __u64 - offset into file
-                ('addr2', ctypes.c_uint64)]  # __u64
+    _fields_ = (('off',   ctypes.c_uint64),  # __u64 - offset into file
+                ('addr2', ctypes.c_uint64))  # __u64
 
 
 class _union_two(ctypes.Union):
-    _fields_ = [('rw_flags',         ctypes.c_int),     # typedef int __kernel_rwf_t
+    _fields_ = (('rw_flags',         ctypes.c_int),     # typedef int __kernel_rwf_t
                 ('fsync_flags',      ctypes.c_uint32),  # __u32
                 ('poll_events',      ctypes.c_uint16),  # __u16
                 ('sync_range_flags', ctypes.c_uint32),  # __u32
@@ -105,44 +105,44 @@ class _union_two(ctypes.Union):
                 ('accept_flags',     ctypes.c_uint32),  # __u32
                 ('cancel_flags',     ctypes.c_uint32),  # __u32
                 ('open_flags',       ctypes.c_uint32),  # __u32
-                ('statx_flags',      ctypes.c_uint32)]  # __u32
+                ('statx_flags',      ctypes.c_uint32))  # __u32
 
 
 class _union_three(ctypes.Union):
-    _fields_ = [('buf_index', ctypes.c_uint16),      # __u16 - index into fixed buffers, if used
-                ('__pad2',    ctypes.c_uint64 * 3)]  # __u64  __pad2[3]
+    _fields_ = (('buf_index', ctypes.c_uint16),         # __u16 - index into fixed buffers, if used
+                ('__pad2',    ctypes.c_uint64 * 3))     # __u64  __pad2[3]
 
 
 # IO submission data structure (Submission Queue Entry)
 # -----------------------------------------------------
 class io_uring_sqe(ctypes.Structure):
     _anonymous_ = ('_union_one', '_union_two', '_union_three')
-    _fileds_ = [
-        ('opcode',        ctypes.c_uint8),   # __u8  - type of operation for this sqe
-        ('flags',         ctypes.c_uint8),   # __u8  - IOSQE_ flags
-        ('ioprio',        ctypes.c_uint16),  # __u16 - ioprio for the request
-        ('fd',            ctypes.c_int32),   # __s32 - file descriptor to do IO on
-        ('_union_one',    _union_one),       # union
-        ('addr',          ctypes.c_uint64),  # __u64 - pointer to buffer or iovecs
-        ('len',           ctypes.c_uint32),  # __u32 - buffer size or number of iovecs
-        ('_union_two',    _union_two),       # union
-        ('user_data',     ctypes.c_uint64),  # __u64 - data to be passed back at completion time
-        ('_union_three',  _union_three)      # union
-    ]
+    _fields_ = (
+        ('opcode',        ctypes.c_uint8),      # __u8  - type of operation for this sqe
+        ('flags',         ctypes.c_uint8),      # __u8  - IOSQE_ flags
+        ('ioprio',        ctypes.c_uint16),     # __u16 - ioprio for the request
+        ('fd',            ctypes.c_int32),      # __s32 - file descriptor to do IO on
+        ('_union_one',    _union_one),          # union
+        ('addr',          ctypes.c_uint64),     # __u64 - pointer to buffer or iovecs
+        ('len',           ctypes.c_uint32),     # __u32 - buffer size or number of iovecs
+        ('_union_two',    _union_two),          # union
+        ('user_data',     ctypes.c_uint64),     # __u64 - data to be passed back at completion time
+        ('_union_three',  _union_three)         # union
+    )
 
 
 # IO completion data structure (Completion Queue Entry)
 # -----------------------------------------------------
 class io_uring_cqe(ctypes.Structure):
-    _fileds_ = [('user_data',   ctypes.c_uint64),  # __u64  - sqe->data submission passed back
-                ('res',         ctypes.c_int32),   # __s32  - result code for this event
-                ('flags',       ctypes.c_uint32)]  # __u32
+    _fields_ = (('user_data',   ctypes.c_uint64),   # __u64  - sqe->data submission passed back
+                ('res',         ctypes.c_int32),    # __s32  - result code for this event
+                ('flags',       ctypes.c_uint32))   # __u32
 
 
 # Filled with the offset for mmap(2)
 # ----------------------------------
 class io_sqring_offsets(ctypes.Structure):
-    _fileds_ = [('head',         ctypes.c_uint32),  # __u32
+    _fields_ = (('head',         ctypes.c_uint32),  # __u32
                 ('tail',         ctypes.c_uint32),  # __u32
                 ('ring_mask',    ctypes.c_uint32),  # __u32
                 ('ring_entries', ctypes.c_uint32),  # __u32
@@ -150,33 +150,33 @@ class io_sqring_offsets(ctypes.Structure):
                 ('dropped',      ctypes.c_uint32),  # __u32
                 ('array',        ctypes.c_uint32),  # __u32
                 ('resv1',        ctypes.c_uint32),  # __u32
-                ('resv2',        ctypes.c_uint64)]  # __u64
+                ('resv2',        ctypes.c_uint64))  # __u64
 
 
 class io_cqring_offsets(ctypes.Structure):
-    _fileds_ = [('head',         ctypes.c_uint32),     # __u32
-                ('tail',         ctypes.c_uint32),     # __u32
-                ('ring_mask',    ctypes.c_uint32),     # __u32
-                ('ring_entries', ctypes.c_uint32),     # __u32
-                ('overflow',     ctypes.c_uint32),     # __u32
-                ('cqes',         ctypes.c_uint32),     # __u32
-                ('resv',         ctypes.c_uint64 * 2)]  # __u64 resv[2]
+    _fields_ = (('head',         ctypes.c_uint32),      # __u32
+                ('tail',         ctypes.c_uint32),      # __u32
+                ('ring_mask',    ctypes.c_uint32),      # __u32
+                ('ring_entries', ctypes.c_uint32),      # __u32
+                ('overflow',     ctypes.c_uint32),      # __u32
+                ('cqes',         ctypes.c_uint32),      # __u32
+                ('resv',         ctypes.c_uint64 * 2))  # __u64 resv[2]
 
 
 # Passed in for io_uring_setup(2). Copied back with updated info on success
 # -------------------------------------------------------------------------
 class io_uring_params(ctypes.Structure):
-    _fileds_ = [('sq_entries',      ctypes.c_uint32),      # __u32
-                ('cq_entries',      ctypes.c_uint32),      # __u32
-                ('flags',           ctypes.c_uint32),      # __u32
-                ('sq_thread_cpu',   ctypes.c_uint32),      # __u32
-                ('sq_thread_idle',  ctypes.c_uint32),      # __u32
-                ('features',        ctypes.c_uint32),      # __u32
-                ('resv',            ctypes.c_uint32 * 4),  # __u32 resv[4]
-                ('sq_off',          io_sqring_offsets),    # struct io_sqring_offsets
-                ('cq_off',          io_cqring_offsets)]    # struct io_cqring_offsets
+    _fields_ = (('sq_entries',      ctypes.c_uint32),       # __u32
+                ('cq_entries',      ctypes.c_uint32),       # __u32
+                ('flags',           ctypes.c_uint32),       # __u32
+                ('sq_thread_cpu',   ctypes.c_uint32),       # __u32
+                ('sq_thread_idle',  ctypes.c_uint32),       # __u32
+                ('features',        ctypes.c_uint32),       # __u32
+                ('resv',            ctypes.c_uint32 * 4),   # __u32 resv[4]
+                ('sq_off',          io_sqring_offsets),     # struct io_sqring_offsets
+                ('cq_off',          io_cqring_offsets))     # struct io_cqring_offsets
 
 
 class io_uring_files_update(ctypes.Structure):
-    _fileds_ = [('offset', ctypes.c_uint32),                 # __u32
-                ('fds',    ctypes.POINTER(ctypes.c_int32))]  # __s32 *
+    _fields_ = (('offset', ctypes.c_uint32),                    # __u32
+                ('fds',    ctypes.POINTER(ctypes.c_int32)))     # __s32 *
