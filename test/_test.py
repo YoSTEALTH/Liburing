ff4ee0b111a7d7e4854160eed0918bf51159676a
YoSTEALTH <ritesh@stealthcentral.com>
2020-01-07 00:36:04 -0600

    added `test_file_write_read`

--- test/_test.py
+++ test/_test.py
@@ -1,6 +1,5 @@
 import os
 import pytest
-import ctypes
 import liburing
 import liburing.prep
 
@@ -28,17 +27,14 @@ def test_setup_kernel_side_polling():
 
 # File
 # ----
-def test_file(tmpdir):
+def test_file_registration(tmpdir):
     ring = liburing.io_uring()
-    # ring = (liburing.io_uring*1)()
     assert liburing.io_uring_queue_init(1, ring, 0) == 0
     fd1 = os.open(os.path.join(tmpdir, '1.txt'), os.O_CREAT)
     fd2 = os.open(os.path.join(tmpdir, '2.txt'), os.O_CREAT)
     try:
         files = liburing.files_fds(fd1, fd2)
-        # register file
         assert liburing.io_uring_register_files(ring, files, len(files)) == 0
-        # unregister all file(s)
         assert liburing.io_uring_unregister_files(ring) == 0
     finally:
         os.close(fd1)
@@ -46,67 +42,39 @@ def test_file(tmpdir):
         liburing.io_uring_queue_exit(ring)
 
 
-# TODO: Working on
-def test_file_2(tmpdir):
+def test_file_write_read(tmpdir):
+    fd = os.open(os.path.join(tmpdir, '1.txt'), os.O_RDWR | os.O_CREAT, 0o660)
     ring = liburing.io_uring()
-    # ts = liburing.kernel_timespec(1, 0)
-    # assert liburing.io_uring_queue_init(1, ring, liburing.IORING_SETUP_IOPOLL) == 0
-    assert liburing.io_uring_queue_init(1, ring, 0) == 0
-    fd = os.open(os.path.join(tmpdir, '1.txt'), os.O_RDWR | os.O_CREAT)
-    os.write(fd, b'test1tt2t3t5t6t6t8')
-    cqe = ctypes.pointer(liburing.io_uring_cqe())  # completion queue
+    # prepare for writing and reading
+    vecs_write = liburing.prep.iovec_write(b'hello', b'world')
+    hello = bytearray(5)
+    world = bytearray(5)
+    vecs_read = liburing.prep.iovec_read(hello, world)
     try:
-        print('cqe:', cqe)
+        # initialization
+        assert liburing.io_uring_queue_init(2, ring, 0) == 0
 
-        # get an sqe and fill in a READV operation
         sqe = liburing.io_uring_get_sqe(ring)
+        liburing.prep.io_uring_prep_writev(sqe, fd, vecs_write[0], 1, 0)
 
-        buffer = bytearray(5)
-        vecs = liburing.prep.iovec_read(buffer)
+        sqe = liburing.io_uring_get_sqe(ring)
+        liburing.prep.io_uring_prep_writev(sqe, fd, vecs_write[1], 1, 5)
 
-        liburing.prep.io_uring_prep_readv(sqe, fd, vecs, 1, 0)
+        # submit both writes
+        assert liburing.io_uring_submit(ring) == 2
 
-        # tell the kernel we have an sqe ready for consumption
-        result = liburing.io_uring_submit(ring)
+        # read "hello"
+        sqe = liburing.io_uring_get_sqe(ring)
+        liburing.prep.io_uring_prep_readv(sqe, fd, vecs_read[0], 1, 0)
 
-        if result != 1:
-            raise ValueError(f'submit got {result}, wanted 1')
+        # read "world"
+        sqe = liburing.io_uring_get_sqe(ring)
+        liburing.prep.io_uring_prep_readv(sqe, fd, vecs_read[1], 1, 5)
 
-        # wait for the sqe to complete
-        no = liburing.io_uring_wait_cqes(ring, cqe, 1, None, None)
-        if no:
-            raise OSError(-no, os.strerror(-no))
-
-        print('cqe:', cqe)
-
-        assert b'test1' == buffer
+        # submit both reads
+        assert liburing.io_uring_submit(ring) == 2
+        assert hello == b'hello'
+        assert world == b'world'
     finally:
         os.close(fd)
         liburing.io_uring_queue_exit(ring)
-
-
-# TODO
-def test_file_polling_io(tmpdir):
-    ring = liburing.io_uring()
-    sqe = liburing.io_uring_sqe()  # submission queue
-    cqe = liburing.io_uring_cqe()  # completion queue
-
-    assert liburing.io_uring_queue_init(1, ring, liburing.IORING_SETUP_IOPOLL) == 0
-    fd1 = os.open(os.path.join(tmpdir, '1.txt'), os.O_CREAT)
-    fd2 = os.open(os.path.join(tmpdir, '2.txt'), os.O_CREAT)
-    try:
-        files = liburing.files_fds(fd1, fd2)
-        # register file
-        assert liburing.io_uring_register_files(ring, files, len(files)) == 0
-
-        # unregister all file(s)
-        assert liburing.io_uring_unregister_files(ring) == 0
-    finally:
-        os.close(fd1)
-        os.close(fd2)
-        liburing.io_uring_queue_exit(ring)
-
-
-# TODO
-def test_file_kernel_side_polling(tmpdir):
-    pass
