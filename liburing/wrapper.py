6ba5ef84df50d9aa29a49e2d8e9c2bcf36c9a27e
YoSTEALTH <ritesh@stealthcentral.com>
2020-02-08 02:25:04 -0600

    replaced lib.trap_error with trap_error as C callback wasn't working

--- liburing/wrapper.py
+++ liburing/wrapper.py
@@ -1,10 +1,8 @@
 import os
-from ._liburing import ffi
 
 __all__ = ('trap_error',)
 
 
-@ffi.def_extern()
 def trap_error(no):
     '''
         Type
@@ -12,13 +10,34 @@ def trap_error(no):
             return: int
 
         Example
-            >>> lib.trap_error(lib.function_name(*args, *kwargs))
+            >>> trap_error(lib.function_name(*args, *kwargs))
 
         Note
             - Raises appropriate python exception for `-errno` returned by C.
-            - This function is turned into C `lib.trap_error()` by `@ffi.def_extern()`
             - `trap_error` name suggested by _habnabit @ https://github.com/habnabit
     '''
     if no < 0:
         raise OSError(-no, os.strerror(-no))
     return no  # success or value
+
+
+# Note: C callback function fails to raise python
+# from ._liburing import ffi
+# @ffi.def_extern()
+# def trap_error(no):
+#     '''
+#         Type
+#             no:     int
+#             return: int
+
+#         Example
+#             >>> lib.trap_error(lib.function_name(*args, *kwargs))
+
+#         Note
+#             - Raises appropriate python exception for `-errno` returned by C.
+#             - This function is turned into C `lib.trap_error()` by `@ffi.def_extern()`
+#             - `trap_error` name suggested by _habnabit @ https://github.com/habnabit
+#     '''
+#     if no < 0:
+#         raise OSError(-no, os.strerror(-no))
+#     return no  # success or value
