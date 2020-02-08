2a86eba989c3b5c672dc2112d9f9ea0a2a7beada
YoSTEALTH <ritesh@stealthcentral.com>
2020-02-07 19:15:53 -0600

    renamed `.prep` to `.interface`

--- liburing/__init__.py
+++ liburing/__init__.py
@@ -2,13 +2,13 @@ from ._liburing import ffi, lib  # noqa
 from ._liburing.lib import *  # noqa
 from .wrapper import *  # noqa - initialization
 from .helper import *  # noqa 
-from .prep import *  # noqa 
+from .interface import *  # noqa 
 # note:
-#   - `.prep` module functions will override `lib` functions for better user experience.
+#   - `.interface` module functions will override `lib` functions for better user experience.
 #   - `lib` and `ffi` is provided for those that like to tinker with cffi.
 '''
     This is a Python wrapper around liburing C library,
     which is a helper to setup and tear-down io_uring instances.
 '''
 __liburing__ = '0.3.0'
-__version__ = '2020.2.6'
+__version__ = '2020.2.7'
