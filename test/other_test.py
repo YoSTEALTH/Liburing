d3cda028a2e8d6be62d22ec1cb8e4756ba847484
YoSTEALTH <ritesh@stealthcentral.com>
2020-02-08 02:24:05 -0600

    working python trap_error function test vs C

--- test/other_test.py
+++ test/other_test.py
@@ -1,28 +1,41 @@
-# import os
-# import pytest
-from liburing import lib
+import pytest
+from liburing import trap_error
 
 
 def test_trap_error():
-    assert lib.trap_error(0) == 0
-    assert lib.trap_error(1) == 1
-    assert lib.trap_error(222) == 222
+    assert trap_error(0) == 0
+    assert trap_error(1) == 1
+    assert trap_error(222) == 222
 
-    # with pytest.raises(BlockingIOError):
-    #     lib.trap_error(-11)
-    # ^^^ THIS FAILS!!! even though it outputs:
-    # -----------------------------------------
-    #     ../../Liburing/Liburing/test/other_test.py From cffi callback <function trap_error at 0x7f3d8f0bc0d0>:
-    #     Traceback (most recent call last):
-    #       File "... /CFFI-Liburing/Liburing/liburing/wrapper.py", line 23, in trap_error
-    #         raise OSError(-no, os.strerror(-no))
-    #     BlockingIOError: [Errno 11] Resource temporarily unavailable
+    with pytest.raises(BlockingIOError):
+        trap_error(-11)
 
-    # THIS WORKS!!! - so something do with cffi
-    # try:
-    #     no = -11
-    #     raise OSError(-no, os.strerror(-no))
-    # except BlockingIOError:
-    #     assert True
-    # else:
-    #     assert False
+
+# import os
+# import pytest
+# from liburing import lib
+
+
+# def test_trap_error():
+#     assert lib.trap_error(0) == 0
+#     assert lib.trap_error(1) == 1
+#     assert lib.trap_error(222) == 222
+
+#     with pytest.raises(BlockingIOError):
+#         lib.trap_error(-11)
+#     # ^^^ THIS FAILS!!! even though it outputs:
+#     # -----------------------------------------
+#     #     ../../Liburing/Liburing/test/other_test.py From cffi callback <function trap_error at 0x7f3d8f0bc0d0>:
+#     #     Traceback (most recent call last):
+#     #       File "... /CFFI-Liburing/Liburing/liburing/wrapper.py", line 23, in trap_error
+#     #         raise OSError(-no, os.strerror(-no))
+#     #     BlockingIOError: [Errno 11] Resource temporarily unavailable
+
+#     # THIS WORKS!!! - so something do with cffi
+#     # try:
+#     #     no = -11
+#     #     raise OSError(-no, os.strerror(-no))
+#     # except BlockingIOError:
+#     #     assert True
+#     # else:
+#     #     assert False
