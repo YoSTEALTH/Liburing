e1663a0dc1fb478791b3b773189aa9711107b1fb
YoSTEALTH <ritesh@stealthcentral.com>
2020-02-08 22:10:39 -0600

    test events

---
+++ test/test_event.py
@@ -0,0 +1,37 @@
+import os
+import os.path
+import liburing
+
+
+def test_event(tmpdir):
+    fd = os.open(os.path.join(tmpdir, 'event.txt'), os.O_RDWR | os.O_CREAT, 0o660)
+    ring = liburing.io_uring()
+
+    # prepare for writing.
+    data = bytearray(b'hello world')
+    vecs = liburing.iovec(data)
+
+    try:
+        # initialization
+        assert liburing.io_uring_queue_init(32, ring, 0) == 0
+
+        # write "hello"
+        sqe = liburing.io_uring_get_sqe(ring)
+        liburing.io_uring_prep_writev(sqe, fd, vecs, 1, 0)
+        liburing.io_uring_submit(ring)
+
+        cqes = liburing.io_uring_cqes()
+        while True:
+            try:
+                assert liburing.io_uring_peek_cqe(ring, cqes) >= 0
+            except BlockingIOError:
+                print('waiting ...')
+            else:
+                print('Finished.')
+                break
+
+        cqe = liburing.io_uring_cqe()
+        liburing.io_uring_cqe_seen(ring, cqe)
+    finally:
+        os.close(fd)
+        liburing.io_uring_queue_exit(ring)
