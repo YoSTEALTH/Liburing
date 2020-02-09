fbbc1ebe66f371d38dd9e22aef5069f20e8b229f
YoSTEALTH <ritesh@stealthcentral.com>
2020-02-09 00:13:06 -0600

    moving `io_using_cqe_seen` to proper position

--- test/test_event.py
+++ test/test_event.py
@@ -18,20 +18,21 @@ def test_event(tmpdir):
         # write "hello"
         sqe = liburing.io_uring_get_sqe(ring)
         liburing.io_uring_prep_writev(sqe, fd, vecs, 1, 0)
-        liburing.io_uring_submit(ring)
+        assert liburing.io_uring_submit(ring) == 1
 
+        print()
         cqes = liburing.io_uring_cqes()
         while True:
             try:
-                assert liburing.io_uring_peek_cqe(ring, cqes) >= 0
+                assert liburing.io_uring_peek_cqe(ring, cqes) == 0
             except BlockingIOError:
-                print('waiting ...')
+                print('waiting')
+                # could `await task(...)` here.
             else:
-                print('Finished.')
+                cqe = liburing.io_uring_cqe()
+                liburing.io_uring_cqe_seen(ring, cqe)
+                print('task done')
                 break
-
-        cqe = liburing.io_uring_cqe()
-        liburing.io_uring_cqe_seen(ring, cqe)
 
         # confirm
         content = os.read(fd, 100)
