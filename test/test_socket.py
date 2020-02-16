a0442ea768c49c3f847baad97334632dc524516e
YoSTEALTH <ritesh@stealthcentral.com>
2020-02-16 05:55:27 -0600

    Linux5.5+ socket accept and timeout feature tested

--- test/test_socket.py
+++ test/test_socket.py
@@ -2,22 +2,21 @@ import socket
 import liburing
 
 
-def test_socket():
+def test_socket_timeout():
     ring = liburing.io_uring()
 
     # socket
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, True)
-    server.setblocking(False)
-    server.bind(('0.0.0.0', 3000))
+    server.bind(('0.0.0.0', 0))  # random port
     server.listen(32)
 
     # prepare
     fd = server.fileno()
     addr = liburing.ffi.new('struct sockaddr *')
     addrlen = liburing.ffi.new('socklen_t *', liburing.ffi.sizeof(addr))
-
+    print()
     try:
         # initialization
         assert liburing.io_uring_queue_init(32, ring, 0) == 0
@@ -25,28 +24,39 @@ def test_socket():
         # accept
         sqe = liburing.io_uring_get_sqe(ring)
         liburing.io_uring_prep_accept(sqe, fd, addr, addrlen, 0)
-        assert liburing.io_uring_submit(ring) == 1
+        sqe.flags |= liburing.IOSQE_IO_LINK
+        sqe.user_data = 1
 
-        print()
+        sqe = liburing.io_uring_get_sqe(ring)
+        ts = liburing.timespec(0, 1000000//24)  # nanosecond
+        liburing.io_uring_prep_link_timeout(sqe, ts, 0)
+        sqe.user_data = 2
+
+        assert liburing.io_uring_submit(ring) == 2
+
         while True:
             try:
                 cqes = liburing.io_uring_cqes()
                 assert liburing.io_uring_peek_cqe(ring, cqes) == 0
-
             except BlockingIOError:
                 print('waiting')
             else:
                 cqe = cqes[0]
+                if cqe.user_data == 1:
+                    # OSError: [Errno 125] Operation canceled
+                    assert cqe.res == -125
+                    liburing.io_uring_cqe_seen(ring, cqe)
+                    continue
+                else:
+                    # Timeout value confirm
+                    assert cqe.user_data == 2
 
-                print('res:', cqe.res)
-                print('user_data:', cqe.user_data)
+                    # `OSError: [Errno 62] Timer expired` or
+                    # `BlockingIOError: [Errno 114] Operation already in progress`
+                    assert cqe.res == -62 or -114
+                    liburing.io_uring_cqe_seen(ring, cqe)
+                    break
 
-                # Error: OSError: [Errno 22] Invalid argument
-                assert cqe.res >= 0
-
-                liburing.io_uring_cqe_seen(ring, cqe)
-                print('task done')
-                break
     finally:
         server.close()
         liburing.io_uring_queue_exit(ring)
