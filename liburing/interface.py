a25188fbb350cb2816eea76680bb0c86325d20fb
YoSTEALTH <ritesh@stealthcentral.com>
2020-02-25 07:32:59 -0600

    added 0.4.0 function that require `trap_error` features.

--- liburing/interface.py
+++ liburing/interface.py
@@ -5,17 +5,50 @@ from .helper import timespec, sigmask
 
 # Library interface
 # -----------------
+def io_uring_opcode_supported(p, op):
+    '''
+        Type
+            p:      io_uring_probe
+            op:     int
+            return: int
+
+        Version
+            0.4.0
+    '''
+    return trap_error(lib.io_uring_opcode_supported(p, op))
+
+
 def io_uring_queue_init_params(entries, ring, p):
     '''
-        ...
+        Type
+            entries:  int
+            ring:     io_uring
+            p:        io_uring_params
+            return:   int
     '''
+    if entries < 1:
+        _ = 'io_uring_queue_init_params(entries) can not be < 1'
+        raise ValueError(_)
     return trap_error(lib.io_uring_queue_init_params(entries, ring, p))
 
 
 def io_uring_queue_init(entries, ring, flags=0):
     '''
-        ...
+        Example
+            >>> ring = io_uring()
+            >>> io_uring_queue_init(1024, ring, 0)
+            >>> io_uring_queue_init(1024, ring, IORING_SETUP_IOPOLL)
+            >>> io_uring_queue_init(1024, ring, IORING_SETUP_SQPOLL)
+
+        Type
+            entries:  int
+            ring:     io_uring
+            flags:    int
+            return:   int
     '''
+    if entries < 1:
+        _ = 'io_uring_queue_init(entries) can not be < 1'
+        raise ValueError(_)
     return trap_error(lib.io_uring_queue_init(entries, ring, flags))
 
 
@@ -24,6 +57,18 @@ def io_uring_queue_mmap(fd, p, ring):
         ...
     '''
     return trap_error(lib.io_uring_queue_mmap(fd, p, ring))
+
+
+def io_uring_ring_dontfork(ring):
+    '''
+        Type
+            ring:   io_uring
+            return: int
+
+        Version
+            0.4.0
+    '''
+    return trap_error(lib.io_uring_ring_dontfork(ring))
 
 
 def io_uring_peek_batch_cqe(ring, cqes, count):
@@ -126,6 +171,45 @@ def io_uring_unregister_eventfd(ring):
     return trap_error(lib.io_uring_unregister_eventfd(ring))
 
 
+def io_uring_register_probe(ring, p, nr):
+    '''
+        Type
+            ring:    io_uring
+            p:       io_uring_probe
+            nr:      int
+            return:  int
+
+        Version
+            0.4.0
+    '''
+    return trap_error(lib.io_uring_register_probe(ring, p, nr))
+
+
+def io_uring_register_personality(ring):
+    '''
+        Type
+            ring:    io_uring
+            return:  int
+
+        Version
+            0.4.0
+    '''
+    return trap_error(lib.io_uring_register_personality(ring))
+
+
+def io_uring_unregister_personality(ring, id):
+    '''
+        Type
+            ring:    io_uring
+            id:      int
+            return:  int
+
+        Version
+            0.4.0
+    '''
+    return trap_error(lib.io_uring_unregister_personality(ring, id))
+
+
 # Peek & Wait
 # -----------
 def io_uring_wait_cqe_nr(ring, cqe_ptr, wait_nr):
@@ -154,15 +238,3 @@ def io_uring_wait_cqe(ring, cqe_ptr):
             cqe_ptr filled in on success, `-errno` on failure.
     '''
     return trap_error(lib.io_uring_wait_cqe(ring, cqe_ptr))
-
-
-# Prep Functions
-# --------------
-def io_uring_prep_readv(sqe, fd, iovecs, nr_vecs, offset, flags=0):
-    lib.io_uring_prep_readv(sqe, fd, iovecs, nr_vecs, offset)
-    sqe.rw_flags = flags
-
-
-def io_uring_prep_writev(sqe, fd, iovecs, nr_vecs, offset, flags=0):
-    lib.io_uring_prep_writev(sqe, fd, iovecs, nr_vecs, offset)
-    sqe.rw_flags = flags
