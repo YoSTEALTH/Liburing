3d6a7ca0aaafb7d306de93c22e9c04966b35559f
YoSTEALTH <ritesh@stealthcentral.com>
2020-02-25 11:49:42 -0600

    pre-release

--- liburing/__init__.py
+++ liburing/__init__.py
@@ -2,14 +2,14 @@ from ._liburing import ffi, lib  # noqa
 from ._liburing.lib import *  # noqa
 from .interface import *  # noqa 
 from .helper import *  # noqa 
-# note:
-#   - `.interface` module functions will override `lib` functions for better user experience.
-#   - `lib` and `ffi` is provided for those that like to tinker with cffi.
 '''
     This is a Python wrapper around liburing C library,
     which is a helper to setup and tear-down io_uring instances.
 '''
-# enables `help(liburing)` to display everything
+# note:
+#   - `.interface` module functions will override `lib` functions for better user experience.
+#   - `lib` and `ffi` is provided for those that like to tinker with cffi.
+#   - enables `help(liburing)` to display everything
 __all__ = [i for i in locals().keys() if not i.startswith('_')]
 __liburing__ = '0.4.0'
 __version__ = '2020.2.25'
