3d46386f1b524660132a3ebfb34f2335959b0411
YoSTEALTH <ritesh@stealthcentral.com>
2020-03-06 19:26:42 -0600

    Fixed, there was a typo in C liburing regarding `io_uring_wait_cqes`

--- liburing/interface.py
+++ liburing/interface.py
@@ -99,8 +99,8 @@ def io_uring_wait_cqes(ring, cqe_ptr, wait_nr, ts=NULL, sm=NULL):
             that an `sqe` is used internally to handle the timeout. Applications using
             this function must never set `sqe->user_data` to `LIBURING_UDATA_TIMEOUT`!
 
-            Note that the application need not call `io_uring_submit()` before calling
-            this function, as we will do that on its behalf. From this it also follows
+            If 'ts' is specified, the application need not call `io_uring_submit()` before
+            calling this function, as we will do that on its behalf. From this it also follows
             that this function isn't safe to use for applications that split SQ and CQ
             handling between two threads and expect that to work without synchronization,
             as this function manipulates both the SQ and CQ side.
