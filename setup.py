fd02819c2c7e995fcc74e02ccfed078e33dcb879
YoSTEALTH <ritesh@stealthcentral.com>
2020-07-11 02:39:36 -0600

    setup: python 3.9 support

--- setup.py
+++ setup.py
@@ -32,4 +32,5 @@ setup(url='https://github.com/YoSTEALTH/Liburing',
                    'Programming Language :: Python :: 3.6',
                    'Programming Language :: Python :: 3.7',
                    'Programming Language :: Python :: 3.8',
+                   'Programming Language :: Python :: 3.9',
                    'Topic :: Software Development :: Libraries :: Python Modules'])
