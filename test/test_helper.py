b2a4e7702fd4af6959a7860a0417844e3c267534
YoSTEALTH <ritesh@stealthcentral.com>
2021-06-11 05:55:08 -0600

    test_helper: added supported for multiple submits, wait, results.

--- test/test_helper.py
+++ test/test_helper.py
@@ -1,13 +1,23 @@
-from liburing import io_uring_submit, io_uring_wait_cqe, trap_error, io_uring_cqe_seen
+from liburing import io_uring_submit, io_uring_wait_cqe, io_uring_wait_cqes, trap_error, io_uring_cqe_seen
 
 
 __all__ = 'submit_wait_result'
 
 
-def submit_wait_result(ring, cqes):
+def submit_wait_result(ring, cqes, no=1):
     io_uring_submit(ring)
-    io_uring_wait_cqe(ring, cqes)
-    cqe = cqes[0]
-    result = trap_error(cqe.res)
-    io_uring_cqe_seen(ring, cqe)
-    return result  # type: int
+    if no == 1:
+        io_uring_wait_cqe(ring, cqes)
+        cqe = cqes[0]
+        result = trap_error(cqe.res)
+        io_uring_cqe_seen(ring, cqe)
+        return result  # type: int
+    else:  # multiple
+        r = []
+        for i in range(no, 0, -1):
+            io_uring_wait_cqes(ring, cqes, i)
+            cqe = cqes[0]
+            result = trap_error(cqe.res)
+            io_uring_cqe_seen(ring, cqe)
+            r.append(result)
+        return r  # type: list[int]
