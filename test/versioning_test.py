4058d030fc5041b1bd5640b6fd695a4f43eada38
YoSTEALTH <ritesh@stealthcentral.com>
2021-06-12 02:19:34 -0600

    versioning_test: trying to match what other OS version would looks like

--- test/versioning_test.py
+++ test/versioning_test.py
@@ -3,7 +3,7 @@ from liburing import skip_it
 
 def test_skip_it():
     assert not skip_it('5.1')
-    assert skip_it('5.11', 'windos')
+    assert skip_it('10-10.0', 'Windows')
     assert not skip_it('5.11', 'linux')
     assert not skip_it('5.11', 'LINUx')
     assert skip_it('5.10004', 'LINUx')
