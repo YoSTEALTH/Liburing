ff294f725f99601d71e7d62cf05a410d76620ce5
YoSTEALTH <ritesh@stealthcentral.com>
2021-06-12 01:34:40 -0600

    rename_test: cleaned up and added `skip_it` check

--- test/rename_test.py
+++ test/rename_test.py
@@ -1,42 +1,35 @@
-import sys
-import os.path
-import pytest
-from platform import uname
-if sys.version_info < (3, 10):
-    from distutils.version import LooseVersion
-else:
-    from setuptools._distutils.version import LooseVersion
-import liburing
+from os.path import join, exists
+from pytest import mark
+from liburing import AT_FDCWD, io_uring, io_uring_cqes, io_uring_queue_init, \
+                     io_uring_get_sqe, io_uring_queue_exit, io_uring_prep_renameat, skip_it
+from test_helper import submit_wait_result
 
 
-required = '5.11'
-skip = LooseVersion(uname().release) < LooseVersion(required)
+version = '5.11'
 
 
-@pytest.mark.skipif(skip, reason=f'requires linux {required}+')
+@mark.skipif(skip_it(version), reason=f'Requires Linux {version}+')
 def test_rename_file(tmpdir):
-    src_file_path = os.path.join(tmpdir, 'src_file.txt').encode()
-    dst_file_path = os.path.join(tmpdir, 'dst_file.txt').encode()
-    with open(src_file_path, 'x'):  # create src file
+    src_file_path = join(tmpdir, 'src_file.txt').encode()
+    dst_file_path = join(tmpdir, 'dst_file.txt').encode()
+
+    # create src file
+    with open(src_file_path, 'x'):
         pass
 
-    flags = 0
-    ring = liburing.io_uring()
+    ring = io_uring()
+    cqes = io_uring_cqes()
     try:
-        assert liburing.io_uring_queue_init(2, ring, 0) == 0
+        assert io_uring_queue_init(2, ring, 0) == 0
 
-        sqe = liburing.get_sqe(ring)
-        liburing.io_uring_prep_renameat(sqe,
-                                        liburing.AT_FDCWD, src_file_path,
-                                        liburing.AT_FDCWD, dst_file_path,
-                                        flags)
-        assert liburing.io_uring_submit_and_wait(ring, 1) == 1
-        liburing.io_uring_cq_advance(ring, 1)
+        assert exists(src_file_path)
+        assert not exists(dst_file_path)
 
-        assert not os.path.exists(src_file_path)  # old file should not exist
-        assert os.path.exists(dst_file_path)      # renamed file should exist
+        sqe = io_uring_get_sqe(ring)
+        io_uring_prep_renameat(sqe, AT_FDCWD, src_file_path, AT_FDCWD, dst_file_path, 0)
+        assert submit_wait_result(ring, cqes) == 0
 
+        assert not exists(src_file_path)  # old file should not exist
+        assert exists(dst_file_path)      # renamed file should exist
     finally:
-        liburing.io_uring_queue_exit(ring)
-
-    
+        io_uring_queue_exit(ring)
