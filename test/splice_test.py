d3ea7a7c817bfec8aec524926d97d18e6e08c712
YoSTEALTH <ritesh@stealthcentral.com>
2021-06-12 02:37:34 -0600

    splice_test: added version check and cleaned up test.

--- test/splice_test.py
+++ test/splice_test.py
@@ -1,55 +1,48 @@
 import os
-import liburing
+from os.path import join
+from pytest import mark
+from liburing import SPLICE_F_MOVE, SPLICE_F_MORE, IOSQE_IO_LINK, io_uring, io_uring_cqes, io_uring_queue_init, \
+                     io_uring_get_sqe, io_uring_prep_splice, io_uring_queue_exit, skip_it
+from test_helper import submit_wait_result
 
 
+version = '5.7'
+
+
+@mark.skipif(skip_it(version), reason=f'Requires Linux {version}+')
 def test_clone_file_using_splice(tmpdir):
-    fd_in = os.open(os.path.join(tmpdir, '1.txt'), os.O_RDWR | os.O_CREAT, 0o660)
-    fd_out = os.open(os.path.join(tmpdir, '2.txt'), os.O_RDWR | os.O_CREAT, 0o660)
-    flags = liburing.SPLICE_F_MOVE | liburing.SPLICE_F_MORE
+    fd_in = os.open(join(tmpdir, '1.txt'), os.O_RDWR | os.O_CREAT, 0o660)
+    fd_out = os.open(join(tmpdir, '2.txt'), os.O_RDWR | os.O_CREAT, 0o660)
+    flags = SPLICE_F_MOVE | SPLICE_F_MORE
     data = b'hello world'
     BUF_SIZE = len(data)
     os.write(fd_in, data)
     r, w = os.pipe()
 
-    ring = liburing.io_uring()
-    cqes = liburing.io_uring_cqes(2)
+    ring = io_uring()
+    cqes = io_uring_cqes(2)
 
     try:
         # initialization
-        assert liburing.io_uring_queue_init(2, ring, 0) == 0
+        assert io_uring_queue_init(2, ring, 0) == 0
 
         # read from file "1.txt"
-        sqe = liburing.io_uring_get_sqe(ring)
-        liburing.io_uring_prep_splice(sqe, fd_in, 0, w, -1, BUF_SIZE, flags)
+        sqe = io_uring_get_sqe(ring)
+        io_uring_prep_splice(sqe, fd_in, 0, w, -1, BUF_SIZE, flags)
         sqe.user_data = 1
 
         # chain top and bottom sqe
-        sqe.flags |= liburing.IOSQE_IO_LINK
+        sqe.flags |= IOSQE_IO_LINK
 
         # write to file "2.txt"
-        sqe = liburing.io_uring_get_sqe(ring)
-        liburing.io_uring_prep_splice(sqe, r, -1, fd_out, 0, BUF_SIZE, flags)
+        sqe = io_uring_get_sqe(ring)
+        io_uring_prep_splice(sqe, r, -1, fd_out, 0, BUF_SIZE, flags)
         sqe.user_data = 2
 
-        # submit both
-        assert liburing.io_uring_submit(ring) == 2
-
-        # wait for ``2`` entry to complete using single syscall
-        assert liburing.io_uring_wait_cqes(ring, cqes, 2) == 0
-        cqe = cqes[0]
-        assert cqe.res == BUF_SIZE
-        assert cqe.user_data == 1
-        liburing.io_uring_cqe_seen(ring, cqe)
-
-        # re-uses the same resources from above?!
-        assert liburing.io_uring_wait_cqes(ring, cqes, 2) == 0
-        cqe = cqes[0]
-        assert cqe.res == BUF_SIZE
-        assert cqe.user_data == 2
-        liburing.io_uring_cqe_seen(ring, cqe)
+        assert submit_wait_result(ring, cqes, 2) == [BUF_SIZE, BUF_SIZE]
         assert os.read(fd_out, BUF_SIZE) == data
 
     finally:
         os.close(fd_in)
         os.close(fd_out)
-        liburing.io_uring_queue_exit(ring)
+        io_uring_queue_exit(ring)
