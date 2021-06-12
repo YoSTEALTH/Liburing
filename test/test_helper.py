f2dfbc80272411026c1f05fc307019559c59b938
YoSTEALTH <ritesh@stealthcentral.com>
2021-06-12 05:21:33 -0600

    test_helper: `submit_wait_result` should `trap_error` after `cqe` has been seen using `io_uring_cqe_seen`.

--- test/test_helper.py
+++ test/test_helper.py
@@ -9,15 +9,18 @@ def submit_wait_result(ring, cqes, no=1):
     if no == 1:
         io_uring_wait_cqe(ring, cqes)
         cqe = cqes[0]
-        result = trap_error(cqe.res)
+        result = cqe.res
         io_uring_cqe_seen(ring, cqe)
-        return result  # type: int
+        return trap_error(result)  # type: int
     else:  # multiple
         r = []
         for i in range(no, 0, -1):
             io_uring_wait_cqes(ring, cqes, i)
             cqe = cqes[0]
-            result = trap_error(cqe.res)
+            r.append(cqe.res)
             io_uring_cqe_seen(ring, cqe)
-            r.append(result)
+
+        # check for errors and raise if any
+        for i in r:
+            trap_error(i)
         return r  # type: list[int]
