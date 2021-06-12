19d9878c81f51a196cc07306b564821d23949bcc
YoSTEALTH <ritesh@stealthcentral.com>
2021-06-12 01:39:02 -0600

    file_openat2_test: improved version checking for feature with `skip_it`

--- test/file_openat2_test.py
+++ test/file_openat2_test.py
@@ -1,19 +1,23 @@
 from os import O_CREAT, O_RDWR
 from os.path import join
-from liburing import AT_FDCWD, RESOLVE_CACHED, IOSQE_IO_LINK, io_uring, io_uring_cqes, iovec, io_uring_queue_init, \
+from pytest import mark
+from liburing import AT_FDCWD, RESOLVE_CACHED, io_uring, io_uring_cqes, iovec, io_uring_queue_init, \
                      io_uring_get_sqe, io_uring_prep_openat2, io_uring_prep_write, io_uring_prep_read, \
                      io_uring_prep_close, io_uring_queue_exit, io_uring_register_buffers, \
-                     open_how
+                     open_how, skip_it
 from test_helper import submit_wait_result
 
 
+version = '5.6'
+
+
+@mark.skipif(skip_it(version), reason=f'Requires Linux {version}+')
 def test_openat2(tmpdir):
     ring = io_uring()
     cqes = io_uring_cqes()
     # prep
     buffers = (bytearray(b'hello world'), bytearray(11))
     iov = iovec(*buffers)
-    
     file_path = join(tmpdir, 'openat2_test.txt').encode()
     try:
         assert io_uring_queue_init(2, ring, 0) == 0
@@ -38,19 +42,21 @@ def test_openat2(tmpdir):
         # confirm
         assert buffers[0] == buffers[1]
 
-        # second open for resolve test
-        how = open_how(O_RDWR, 0, RESOLVE_CACHED)
-        sqe = io_uring_get_sqe(ring)
-        io_uring_prep_openat2(sqe, AT_FDCWD, file_path, how)
-        fd2 = submit_wait_result(ring, cqes)
-
-        # close both files
+        # close
         sqe = io_uring_get_sqe(ring)
         io_uring_prep_close(sqe, fd)
-        sqe.flags |= IOSQE_IO_LINK
-        sqe = io_uring_get_sqe(ring)
-        io_uring_prep_close(sqe, fd2)
-        submit_wait_result(ring, cqes, 2)
+        submit_wait_result(ring, cqes)
 
+        if not skip_it('5.12'):
+            # second open for resolve test
+            how = open_how(O_RDWR, 0, RESOLVE_CACHED)
+            sqe = io_uring_get_sqe(ring)
+            io_uring_prep_openat2(sqe, AT_FDCWD, file_path, how)
+            fd2 = submit_wait_result(ring, cqes)
+
+            # close `fd2`
+            sqe = io_uring_get_sqe(ring)
+            io_uring_prep_close(sqe, fd2)
+            submit_wait_result(ring, cqes)
     finally:
         io_uring_queue_exit(ring)
