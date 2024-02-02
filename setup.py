52a0ab567ae7e160a52d7554e71f1a957c1cf069
YoSTEALTH <ritesh@stealthcentral.com>
2024-02-02 12:47:35 -0600

    setup: try old setup

--- setup.py
+++ setup.py
@@ -9,38 +9,34 @@ from Cython.Distutils import Extension
 # manually change this:
 os_liburing = False  # use OS `liburing.so`?
 # note: OS `liburing.so` tend to be outdated! Try it, run test, if no error is raised its good :)
+version = '2024.1.27'
 package = 'liburing'
 threads = cpu_count()//2 or 1  # use half of cpu resources
-sources = ['liburing/*.pyx']
-# sources = ['liburing/version.pyx', 'liburing/helper.pyx']
+sources = 'liburing/*.pyx'
 language = 'c'
 lib_name = f'{package}.*'
 
 # compiler options
 Options.warning_errors = True   # turn all warnings into errors.
-# if __debug__:  # `gcc --help=common` for more info
-#     Options.fast_fail = False
-#     Options.annotate = True  # generate `*.html` file for debugging & optimization purposes.
-#     compile_args = ['Oz', 'g0']
-# else:
-Options.fast_fail = False
-Options.annotate = False
-# compile_args = ['O3', 'g0']
-
-compile_args = []  # bypass
-
+if __debug__:  # `gcc --help=common` for more info
+    Options.fast_fail = False
+    Options.annotate = True  # generate `*.html` file for debugging & optimization purposes.
+    compile_args = ['-Oz', '-g0']
+else:
+    Options.annotate = False
+    Options.fast_fail = True
+    compile_args = ['-O3', '-g0']
 if os_liburing:  # compile using OS `liburing.so`
     extension = [Extension(name=lib_name,  # where the `.so` will be saved.
-                           sources=sources,
+                           sources=[sources],
                            language=language,
                            extra_compile_args=compile_args)]
 else:  # compile `liburing` C library as well.
     path = 'libs/liburing'
-    makefile = 'libs/liburing/Makefile'
     src_path = 'libs/liburing/src'
     inc_path = 'libs/liburing/src/include'
     extension = [Extension(name=lib_name,  # where the `.so` will be saved.
-                           sources=sources,
+                           sources=[sources],
                            language=language,
                            include_dirs=[inc_path],
                            # TODO: see if this will include "includes" folder!
@@ -49,13 +45,24 @@ else:  # compile `liburing` C library as well.
                            libraries=[package[3:]],  # remove `lib` part.
                            library_dirs=[src_path],
                            extra_compile_args=compile_args)]
-    # configure custom C liburing install , capture_output=True
-    sub_process_run(['./configure'], cwd=path)
-    sub_process_run(['make'], cwd=path)
-    sub_process_run(['make', 'install'], cwd=path)
+    # TODO: should not use `check=True` but check for output for error
+    sub_process_run(['./configure'], cwd=path, capture_output=True, check=True)
+    # sub_process_run(['make'], cwd=path, capture_output=True, check=True)
 
-setup(package_data={package: ['*.pxd']},
+setup(name=package,
+      version=version,
+      platforms=['linux'],
+      # packages=[package, f'{package}.includes'],
+      packages=[package],
+      # include header so people can `cimport`
+      # package_data={package: ['*.pxd'], f'{package}.includes': [f'{package}/includes/*.pxd']},
+      # package_data={package: ['*.pxd'], f'{package}.includes': ['*.pxd']},
+      package_data={package: ['*.pxd']},
       ext_modules=cythonize(extension,
                             nthreads=threads,
-                            compiler_directives={'embedsignature': True,  # show all `__doc__`
-                                                 'language_level': '3'}))
+                            compiler_directives={
+                                'embedsignature': True,  # show all `__doc__`
+                                'language_level': '3',
+                                # 'legacy_implicit_noexcept': True  # bug fix?
+                            }),
+      zip_safe=False)  # needed for setuptools
