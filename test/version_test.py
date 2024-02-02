8abb67cd75975610d0b2f17d67730582e1a75153
YoSTEALTH <ritesh@stealthcentral.com>
2024-02-02 17:57:41 -0600

    test: change liburing minor to 6

--- test/version_test.py
+++ test/version_test.py
@@ -3,4 +3,4 @@ from liburing.version import liburing_major_version, liburing_minor_version
 
 def test_liburing_version():
     assert liburing_major_version() == 2
-    assert liburing_minor_version() == 5
+    assert liburing_minor_version() == 6
