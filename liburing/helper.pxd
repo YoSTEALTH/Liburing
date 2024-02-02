398601f90a1344f19cd43f20ad345ce59de19db6
YoSTEALTH <ritesh@stealthcentral.com>
2024-02-02 09:30:46 -0600

    helper: created `trap_error` & `raise_erorr` functions and test.

---
+++ liburing/helper.pxd
@@ -0,0 +1,6 @@
+from libc.errno cimport errno
+from libc.string cimport strerror
+
+
+cpdef int trap_error(int no)
+cpdef void raise_error(signed int no=?)
---
+++ test/helper_test.py
@@ -0,0 +1,13 @@
+from pytest import raises
+from liburing import trap_error
+
+
+def test_trap_error():
+    assert trap_error(0) == 0
+    assert trap_error(1) == 1
+
+    with raises(BlockingIOError):
+        trap_error(-11)
+
+    with raises(OSError):
+        trap_error(-1)
