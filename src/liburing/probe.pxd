11f5e3989e2d3f567b2669fe6b06a0494877ef36
YoSTEALTH <ritesh@stealthcentral.com>
2024-02-13 20:09:33 -0600

    probe: improved `io_uring_probe` class and struct

--- src/liburing/probe.pxd
+++ src/liburing/probe.pxd
@@ -1,5 +1,4 @@
-from .type cimport bool
-from .io_uring cimport io_uring_probe_t
+from .type cimport bool, __u8, __u16, __u32
 from .liburing cimport io_uring_t, io_uring
 
 
@@ -8,6 +7,20 @@ cdef class io_uring_probe:
 
 
 cdef extern from * nogil:
+    struct io_uring_probe_op_t "io_uring_probe_op":
+        __u8    op
+        __u8    resv
+        __u16   flags   # IO_URING_OP_* flags
+        __u32   resv2
+
+    struct io_uring_probe_t "io_uring_probe":
+        __u8    last_op   # last opcode supported
+        __u8    ops_len   # length of ops[] array below
+        __u16   resv
+        __u32   resv2[3]
+        io_uring_probe_op_t   ops[]
+
+
     io_uring_probe_t * io_uring_get_probe_ring_c 'io_uring_get_probe_ring'(io_uring_t *ring)
     io_uring_probe_t * io_uring_get_probe_c 'io_uring_get_probe'()  # note: (void) is removed
     void io_uring_free_probe_c 'io_uring_free_probe'(io_uring_probe_t *probe)
@@ -22,6 +35,3 @@ cpdef io_uring_probe io_uring_get_probe()
 cpdef void io_uring_free_probe(io_uring_probe probe)
 cpdef bool io_uring_opcode_supported(io_uring_probe p, int op)
 cpdef int io_uring_register_probe(io_uring ring, io_uring_probe p, unsigned int nr)
-
-
-cpdef dict probe
--- src/liburing/probe.pyx
+++ src/liburing/probe.pyx
@@ -1,13 +1,30 @@
-from libc.stdlib cimport free
-from .helper cimport trap_error
-from .io_uring cimport IORING_OP_LAST, io_uring_op
+from libc.stdlib cimport calloc, free
+from .error cimport trap_error, memory_error
 
 
 cdef class io_uring_probe:
+    
+    def __cinit__(self, unsigned int num=0):
+        if num:
+            self.ptr = <io_uring_probe_t*>calloc(num, sizeof(io_uring_probe_t))
+            if self.ptr is NULL:
+                memory_error(self)
+
     def __dealloc__(self):
         # just in case user forgets to call `io_uring_free_probe` or error happened 
         if self.ptr is not NULL:
             io_uring_free_probe_c(self.ptr)
+            self.ptr = NULL
+
+    @property
+    def last_op(self):
+        if self.ptr is not NULL:
+            return self.ptr.last_op
+
+    @property
+    def ops_len(self):
+        if self.ptr is not NULL:
+            return self.ptr.ops_len
 
 
 cpdef io_uring_probe io_uring_get_probe_ring(io_uring ring):
@@ -22,8 +39,7 @@ cpdef io_uring_probe io_uring_get_probe():
 
 cpdef void io_uring_free_probe(io_uring_probe probe):
     io_uring_free_probe_c(probe.ptr)
-    if probe.ptr is not NULL:
-        probe.ptr = NULL  # account for C Liburing bug
+    probe.ptr = NULL
 
 cpdef inline bool io_uring_opcode_supported(io_uring_probe p, int op):
     return io_uring_opcode_supported_c(p.ptr, op)
