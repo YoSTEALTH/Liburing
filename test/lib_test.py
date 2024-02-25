0c8c7146ad89312486e7994c72d7d9efacc04d5a
YoSTEALTH <ritesh@stealthcentral.com>
2024-02-24 23:43:38 -0600

    test: added basic `lib_test.py` define test

---
+++ test/lib_test.py
@@ -0,0 +1,8 @@
+import liburing
+
+
+def test_defines():
+    assert liburing.LIBURING_UDATA_TIMEOUT == 18446744073709551615
+    assert liburing.IORING_FILE_INDEX_ALLOC == 4294967295
+    assert liburing.IORING_REGISTER_USE_REGISTERED_RING == 2147483648
+    assert liburing.AT_FDCWD == -100
