c846e10d816eabb1ff758e0d73451d11ba1025fb
YoSTEALTH <ritesh@stealthcentral.com>
2024-02-28 01:26:52 -0600

    test.conftest: created `@pytest.mark.skip_linux` fixture.

--- test/conftest.py
+++ test/conftest.py
@@ -4,6 +4,9 @@ import pytest
 import pathlib
 import getpass
 import tempfile
+import platform
+import functools
+from packaging.version import Version
 
 
 @pytest.fixture
@@ -30,3 +33,45 @@ def tmp_dir():
     if not os.path.exists(path):
         os.mkdir(path)
     return pathlib.Path(tempfile.mkdtemp(dir=path))
+
+
+@functools.lru_cache
+def parse_linux_version(version):
+    '''
+        Example
+            >>> parse_linux_version('6.7.5')
+            '6.7.5'  # <class 'packaging.version.Version'>
+    '''
+    return Version(version)
+
+
+LINUX_VERSION = parse_linux_version(platform.release().split('-')[0])
+
+
+@pytest.fixture(autouse=True)
+def skip_by_platform(request):
+    '''
+        Example
+            >>> @pytest.mark.skip_linux('6.7.5')
+            >>> def test_function():
+            ...
+            test.py::test_function SKIPPED (Linux version `6.7.4 < 6.7.5` required version.)
+
+            >>> @pytest.mark.skip_linux('6.7.5', 'custom message')
+            >>> def test_function():
+            ...
+            test.py::test_function SKIPPED (custom message)
+
+            >>> @pytest.mark.skip_linux('6.7.5', '')
+            >>> def test_function():
+            ...
+            test.py::test_function SKIPPED
+    '''
+    if r := request.node.get_closest_marker('skip_linux'):
+        if LINUX_VERSION < (res := parse_linux_version(r.args[0])):
+            pytest.skip(r.args[1] if len(r.args) > 1 else f'Linux version `{LINUX_VERSION} < {res}` required version.')
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers",
+                            "skip_linux(version, message): skipping linux version not supported.")
