b2c54e9ec8a093e65095eadffb9a3d9b286a01f5
YoSTEALTH <ritesh@stealthcentral.com>
2024-03-03 02:34:38 -0600

    other: added miscellaneous function.

--- src/liburing/other.pxd
+++ src/liburing/other.pxd
@@ -1,20 +1,55 @@
 from .lib.uring cimport *
-from .type cimport sigset
-from .error cimport trap_error
-from .queue cimport io_uring_params
+from .queue cimport io_uring, io_uring_params, io_uring_sqe
 
 
-#  `io_uring` syscalls.
-cpdef int io_uring_enter(unsigned int fd,
-                         unsigned int to_submit,
-                         unsigned int min_complete,
-                         unsigned int flags,
-                         sigset sig) nogil
-cpdef int io_uring_enter2(unsigned int fd,
-                          unsigned int to_submit,
-                          unsigned int min_complete,
-                          unsigned int flags,
-                          sigset sig,
-                          size_t sz) nogil
-cpdef int io_uring_setup(unsigned int entries,
-                         io_uring_params p) nogil
+cdef class io_uring_buf_ring:
+    cdef __io_uring_buf_ring * ptr
+    # TODO: io_uring_free_buf_ring on exit.
+
+
+cpdef io_uring_buf_ring io_uring_setup_buf_ring(io_uring ring,
+                                                unsigned int nentries,
+                                                int bgid,
+                                                unsigned int flags,
+                                                int ret)
+cpdef int io_uring_free_buf_ring(io_uring ring,
+                                 io_uring_buf_ring br,
+                                 unsigned int nentries,
+                                 int bgid)
+
+cpdef void io_uring_prep_fadvise(io_uring_sqe sqe,
+                                 int fd,
+                                 __u64 offset,
+                                 off_t len,
+                                 int advice) noexcept nogil
+# TODO:
+# cpdef void io_uring_prep_madvise(io_uring_sqe sqe,
+#                                         void *addr,
+#                                         off_t length,
+#                                         int advice) noexcept nogil
+
+cpdef void io_uring_prep_msg_ring(io_uring_sqe sqe,
+                                  int fd,
+                                  unsigned int len,
+                                  __u64 data,
+                                  unsigned int flags=?) noexcept nogil
+cpdef void io_uring_prep_msg_ring_fd(io_uring_sqe sqe,
+                                     int fd,
+                                     int source_fd,
+                                     int target_fd,
+                                     __u64 data,
+                                     unsigned int flags=?) noexcept nogil
+cpdef void io_uring_prep_msg_ring_fd_alloc(io_uring_sqe sqe,
+                                           int fd,
+                                           int source_fd,
+                                           __u64 data,
+                                           unsigned int flags=?) noexcept nogil
+cpdef void io_uring_prep_msg_ring_cqe_flags(io_uring_sqe sqe,
+                                            int fd,
+                                            unsigned int len,
+                                            __u64 data,
+                                            unsigned int cqe_flags,
+                                            unsigned int flags=?) noexcept nogil
+
+cpdef ssize_t io_uring_mlock_size(unsigned int entries, unsigned int flags) nogil
+cpdef ssize_t io_uring_mlock_size_params(unsigned int entries, io_uring_params p) nogil
--- src/liburing/other.pyx
+++ src/liburing/other.pyx
@@ -1,19 +1,76 @@
-#  `io_uring` syscalls.
-cpdef int io_uring_enter(unsigned int fd,
-                         unsigned int to_submit,
-                         unsigned int min_complete,
-                         unsigned int flags,
-                         sigset sig) nogil:
-    return trap_error(__io_uring_enter(fd, to_submit, min_complete, flags, sig.ptr))
+from .error cimport trap_error, memory_error
 
-cpdef int io_uring_enter2(unsigned int fd,
-                          unsigned int to_submit,
-                          unsigned int min_complete,
-                          unsigned int flags,
-                          sigset sig,
-                          size_t sz) nogil:
-    return trap_error(__io_uring_enter2(fd, to_submit, min_complete, flags, sig.ptr, sz))
 
-cpdef int io_uring_setup(unsigned int entries,
-                         io_uring_params p) nogil:
-    return trap_error(__io_uring_setup(entries, p.ptr))
+cdef class io_uring_buf_ring:
+    pass
+
+# Mapped buffer ring alloc/register + unregister/free helpers
+cpdef io_uring_buf_ring io_uring_setup_buf_ring(io_uring ring,
+                                                unsigned int nentries,
+                                                int bgid,
+                                                unsigned int flags,
+                                                int ret):
+    cdef io_uring_buf_ring buf = io_uring_buf_ring()
+    buf.ptr = __io_uring_setup_buf_ring(ring.ptr, nentries, bgid, flags, &ret)
+    memory_error(buf)
+    return buf
+
+cpdef int io_uring_free_buf_ring(io_uring ring,
+                                 io_uring_buf_ring br,
+                                 unsigned int nentries,
+                                 int bgid):
+    cdef int r = __io_uring_free_buf_ring(ring.ptr, br.ptr, nentries, bgid)
+    trap_error(r)
+    br.ptr = NULL
+
+cpdef inline void io_uring_prep_fadvise(io_uring_sqe sqe,
+                                        int fd,
+                                        __u64 offset,
+                                        off_t len,
+                                        int advice) noexcept nogil:
+    __io_uring_prep_fadvise(sqe.ptr, fd, offset, len, advice)
+
+# TODO:
+# cpdef inline void io_uring_prep_madvise(io_uring_sqe sqe,
+#                                         void *addr,
+#                                         off_t length,
+#                                         int advice) noexcept nogil:
+#     __io_uring_prep_madvise(sqe.ptr, &addr, length, advice)
+
+
+cpdef inline void io_uring_prep_msg_ring(io_uring_sqe sqe,
+                                         int fd,
+                                         unsigned int len,
+                                         __u64 data,
+                                         unsigned int flags=0) noexcept nogil:
+    __io_uring_prep_msg_ring(sqe.ptr, fd, len, data, flags)
+
+cpdef inline void io_uring_prep_msg_ring_fd(io_uring_sqe sqe,
+                                            int fd,
+                                            int source_fd,
+                                            int target_fd,
+                                            __u64 data,
+                                            unsigned int flags=0) noexcept nogil:
+    __io_uring_prep_msg_ring_fd(sqe.ptr, fd, source_fd, target_fd, data, flags)
+
+cpdef inline void io_uring_prep_msg_ring_fd_alloc(io_uring_sqe sqe,
+                                                  int fd,
+                                                  int source_fd,
+                                                  __u64 data,
+                                                  unsigned int flags=0) noexcept nogil:
+    __io_uring_prep_msg_ring_fd_alloc(sqe.ptr, fd, source_fd, data, flags)
+
+cpdef inline void io_uring_prep_msg_ring_cqe_flags(io_uring_sqe sqe,
+                                                   int fd,
+                                                   unsigned int len,
+                                                   __u64 data,
+                                                   unsigned int cqe_flags,
+                                                   unsigned int flags=0) noexcept nogil:
+    __io_uring_prep_msg_ring_cqe_flags(sqe.ptr, fd, len, data, flags, cqe_flags)
+
+
+cpdef ssize_t io_uring_mlock_size(unsigned int entries, unsigned int flags) nogil:
+    return __io_uring_mlock_size(entries, flags)
+
+cpdef ssize_t io_uring_mlock_size_params(unsigned int entries, io_uring_params p) nogil:
+    return __io_uring_mlock_size_params(entries, p.ptr)
