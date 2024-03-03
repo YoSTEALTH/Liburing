c59e1b8a2c7458c464713d25195745d0686f520c
YoSTEALTH <ritesh@stealthcentral.com>
2024-03-03 02:39:11 -0600

    common: `io_uring_prep_provide_buffers` & `io_uring_prep_remove_buffers`

--- src/liburing/common.pxd
+++ src/liburing/common.pxd
@@ -1,6 +1,15 @@
-from .lib.uring cimport __io_uring_prep_close, __io_uring_prep_close_direct
+from .lib.uring cimport __io_uring_prep_close, __io_uring_prep_close_direct, \
+                        __io_uring_prep_provide_buffers, __io_uring_prep_remove_buffers
 from .queue cimport io_uring_sqe
 
 
 cpdef void io_uring_prep_close(io_uring_sqe sqe, int fd) noexcept nogil
 cpdef void io_uring_prep_close_direct(io_uring_sqe sqe, unsigned int file_index) noexcept nogil
+
+cpdef void io_uring_prep_provide_buffers(io_uring_sqe sqe,
+                                         unsigned char[:] addr,
+                                         int len,
+                                         int nr,
+                                         int bgid,
+                                         int bid=?) noexcept nogil
+cpdef void io_uring_prep_remove_buffers(io_uring_sqe sqe, int nr, int bgid) noexcept nogil
--- src/liburing/common.pyx
+++ src/liburing/common.pyx
@@ -4,3 +4,15 @@ cpdef inline void io_uring_prep_close(io_uring_sqe sqe, int fd) noexcept nogil:
 cpdef inline void io_uring_prep_close_direct(io_uring_sqe sqe,
                                              unsigned int file_index) noexcept nogil:
     __io_uring_prep_close_direct(sqe.ptr, file_index)
+
+
+cpdef inline void io_uring_prep_provide_buffers(io_uring_sqe sqe,
+                                                unsigned char[:] addr,  # void *addr,
+                                                int len,
+                                                int nr,
+                                                int bgid,
+                                                int bid=0) noexcept nogil:
+    __io_uring_prep_provide_buffers(sqe.ptr, &addr[0], len, nr, bgid, bid)
+
+cpdef inline void io_uring_prep_remove_buffers(io_uring_sqe sqe, int nr, int bgid) noexcept nogil:
+    __io_uring_prep_remove_buffers(sqe.ptr, nr, bgid)
