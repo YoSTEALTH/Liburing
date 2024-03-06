3fbe15f97d1fbb4f1acec0557436cf6d4f16a4f9
YoSTEALTH <ritesh@stealthcentral.com>
2024-03-06 02:52:52 -0600

    test.queue: created `test_init_max` to make maximum entries can be utilized. Also linking is used to make sure the `cqe` return in order.

--- test/queue/init_exit_test.py
+++ test/queue/init_exit_test.py
@@ -1,31 +1,62 @@
-from errno import EINVAL
-from pytest import raises
-from liburing import IORING_SETUP_IOPOLL, IORING_SETUP_SQPOLL, \
-                     io_uring, io_uring_queue_init, io_uring_queue_exit
+import errno
+import pytest
+import liburing
 
 
 def test_io_uring_init_exit():
-    ring = io_uring()    
+    ring = liburing.io_uring()    
     assert ring.flags == 0
     assert ring.ring_fd == 0
     assert ring.features == 0
     assert ring.enter_ring_fd == 0
     assert ring.int_flags == 0
-    assert io_uring_queue_init(8, ring, 0) == 0
+    assert liburing.io_uring_queue_init(8, ring, 0) == 0
     assert ring.ring_fd > 0
     assert ring.enter_ring_fd > 0
-    assert io_uring_queue_exit(ring) is None
+    assert liburing.io_uring_queue_exit(ring) is None
     assert str(ring).startswith('io_uring(flags=')
 
-    ring = io_uring()
-    with raises(OSError) as e:  # Invalid argument
-        io_uring_queue_init(0, ring, 0)
-    assert e.value.errno == EINVAL
-    assert io_uring_queue_exit(ring) is None
+    ring = liburing.io_uring()
+    with pytest.raises(OSError) as e:  # Invalid argument
+        liburing.io_uring_queue_init(0, ring, 0)
+    assert e.value.errno == errno.EINVAL
+    assert liburing.io_uring_queue_exit(ring) is None
 
 
 def test_setup_iopoll_sqpoll():
-    for i, setup in enumerate((IORING_SETUP_IOPOLL, IORING_SETUP_SQPOLL), 1):
-        ring = io_uring()
-        assert io_uring_queue_init(1, ring, setup) == 0
-        assert io_uring_queue_exit(ring) is None
+    for i, setup in enumerate((liburing.IORING_SETUP_IOPOLL, liburing.IORING_SETUP_SQPOLL), 1):
+        ring = liburing.io_uring()
+        assert liburing.io_uring_queue_init(1, ring, setup) == 0
+        assert liburing.io_uring_queue_exit(ring) is None
+
+
+def test_init_max():
+    cqe = liburing.io_uring_cqe()
+    ring = liburing.io_uring()
+    maximum = 32768
+    with pytest.raises(OSError):
+        liburing.io_uring_queue_init(maximum+1, ring)
+    liburing.io_uring_queue_init(maximum, ring)
+
+    for i in range(maximum-1):
+        sqe = liburing.io_uring_get_sqe(ring)
+        liburing.io_uring_prep_nop(sqe)
+        sqe.flags = liburing.IOSQE_IO_LINK | liburing.IOSQE_ASYNC
+        sqe.user_data = i
+    else:  # last
+        sqe = liburing.io_uring_get_sqe(ring)
+        liburing.io_uring_prep_nop(sqe)
+        sqe.flags = liburing.IOSQE_ASYNC
+        sqe.user_data = i+1
+
+    assert liburing.io_uring_submit_and_wait_timeout(ring, cqe, maximum) == maximum
+
+    for i in range(maximum):
+        assert cqe[i].user_data == i
+
+    liburing.io_uring_cq_advance(ring, maximum)
+    assert liburing.io_uring_peek_cqe(ring, cqe) == -errno.EAGAIN
+
+    liburing.io_uring_queue_exit(ring)
+    with pytest.raises(ValueError):
+        liburing.io_uring_queue_exit(ring)
