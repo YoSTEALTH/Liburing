046dcce56a6ac37366a04f69ded8750d68c70e26
YoSTEALTH <ritesh@stealthcentral.com>
2024-03-31 18:25:18 -0600

    include.liburing: commenting

--- src/liburing/include/liburing.h
+++ src/liburing/include/liburing.h
@@ -3,4 +3,7 @@
     content before compiling.
 
     This trick is used so `pip` will properly install and uninstall these files.
+
+    Include header files are needed when external library uses `cimport`
+    from `liburing` and that library needs header info to compile properly.
 */
