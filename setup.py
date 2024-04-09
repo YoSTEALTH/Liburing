c1222319d935592941e40f65e843c4d1a2431430
YoSTEALTH <ritesh@stealthcentral.com>
2024-04-09 17:54:24 -0600

    setup: improved `setup` to use `build_extensions` to handle `configure`

--- setup.py
+++ setup.py
@@ -1,64 +1,73 @@
 from os import cpu_count
-from shutil import copy2, copytree
+from shutil import copy2, copytree, rmtree
 from os.path import join
-from tempfile import TemporaryDirectory
+from tempfile import mkdtemp
 from subprocess import run as sub_process_run
 from setuptools import setup
-from setuptools.command.build import build
+from setuptools.command.build_ext import build_ext
 from Cython.Build import cythonize
 from Cython.Compiler import Options
 from Cython.Distutils import Extension
 
 
-threads = cpu_count()
-# compiler options
-Options.annotate = False
-Options.fast_fail = True
-Options.docstrings = True
-Options.warning_errors = False
+class BuildExt(build_ext):
 
-
-class Build(build):
     def initialize_options(self):
         super().initialize_options()
-        self.parallel = threads  # manually set
+        self.parallel = threads
+
+    def build_extensions(self):
+        copytree('libs/liburing/src', libsrc)
+        for src in ('libs/liburing/configure',
+                    'libs/liburing/liburing-ffi.pc.in',
+                    'libs/liburing/liburing.pc.in',
+                    'libs/liburing/liburing.spec',
+                    'libs/liburing/Makefile',
+                    'libs/liburing/Makefile.common',
+                    'libs/liburing/Makefile.quiet'):
+            copy2(src, join(tmpdir, src))
+
+        # sub_process_run(['./configure', '--use-libc'], cwd=lib, capture_output=True, check=True)
+        sub_process_run(['./configure'], cwd=lib, capture_output=True, check=True)
+        sub_process_run(['make', f'--jobs={threads}'], cwd=lib, capture_output=True)
+        # note: just runs `configure` & `make`, does not `install`.
+
+        # replace `include` placeholder files with actual content.
+        copytree(libinc, 'src/liburing/include', dirs_exist_ok=True)
+        super().build_extensions()
 
 
-with TemporaryDirectory() as tmpdir:
-    lib = join(tmpdir, 'libs/liburing')
-    libsrc = join(lib, 'src')
-    libinc = join(libsrc, 'include')
-    copytree('libs/liburing/src', libsrc)
-    for src in ('libs/liburing/configure',
-                'libs/liburing/liburing-ffi.pc.in',
-                'libs/liburing/liburing.pc.in',
-                'libs/liburing/liburing.spec',
-                'libs/liburing/Makefile',
-                'libs/liburing/Makefile.common',
-                'libs/liburing/Makefile.quiet'):
-        copy2(src, join(tmpdir, src))
-
-    sub_process_run(['./configure', '--use-libc'], cwd=lib, capture_output=True, check=True)
-    sub_process_run(['make', f'--jobs={threads}'], cwd=lib, capture_output=True)
-    # note: just runs `configure` & `make`, does not `install`.
-
-    extension = [Extension(name='liburing.*',  # where the `.so` will be saved.
-                           sources=['src/liburing/*.pyx'],
-                           language='c',
-                           libraries=['uring'],
-                           library_dirs=[libsrc],
-                           include_dirs=[libinc],
-                           # optimize & remove debug symbols + data.
-                           extra_compile_args=['-O3', '-g0'])]
-
-    # replace `include` placeholder files with actual content.
-    copytree(libinc, 'src/liburing/include', dirs_exist_ok=True)
-    # install
-    setup(cmdclass={'build': Build},
-          ext_modules=cythonize(extension,
-                                nthreads=threads,
-                                compiler_directives={
-                                    'embedsignature': True,  # show all `__doc__`
-                                    'boundscheck': False,
-                                    'wraparound': False,
-                                    'language_level': 3}))
+if __name__ == '__main__':
+    # uring = 'uring'
+    uring = 'uring-ffi'
+    tmpdir = mkdtemp()
+    threads = cpu_count()
+    # compiler options
+    Options.annotate = False
+    Options.fast_fail = True
+    Options.docstrings = True
+    Options.warning_errors = False
+    try:
+        lib = join(tmpdir, 'libs/liburing')
+        libsrc = join(lib, 'src')
+        libinc = join(libsrc, 'include')
+        extension = [Extension(name='liburing.*',  # where the `.so` will be saved.
+                               sources=['src/liburing/*.pyx'],
+                               language='c',
+                               libraries=[uring],
+                               library_dirs=[libsrc],
+                               include_dirs=[libinc],
+                               # optimize & remove debug symbols + data.
+                               extra_compile_args=['-O3', '-g0'])]
+        # install
+        setup(cmdclass={'build_ext': BuildExt},
+              ext_modules=cythonize(extension,
+                                    nthreads=threads,
+                                    compiler_directives={
+                                        'embedsignature': True,  # show all `__doc__`
+                                        'boundscheck': False,
+                                        'wraparound': False,
+                                        'language_level': 3,
+                                        'show_performance_hints': False}))
+    finally:
+        rmtree(tmpdir)
