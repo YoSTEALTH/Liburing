f5309188c3a2ac4e6c23f8f76a64543ecdc40ef5
YoSTEALTH <ritesh@stealthcentral.com>
2024-06-04 22:15:55 -0600

    test: created `get_cqe_test` to figure out why `io_uring_peek_batch_cqe`
    segfaults!

---
+++ test/queue/get_cqe_test.py
@@ -0,0 +1,40 @@
+import liburing
+
+
+def test_peek_batch(ring, cqe):
+    loop = 300
+    for i in range(loop):
+        sqe = liburing.io_uring_get_sqe(ring)
+        sqe.user_data = i+1
+
+    assert (counter := liburing.io_uring_submit(ring)) == loop
+    liburing.io_uring_wait_cqe_nr(ring, cqe, counter)  # wait for all to be ready
+
+    # >>> difference START
+    cq_ready = liburing.io_uring_peek_batch_cqe(ring, cqe, counter)
+    for i in range(cq_ready):
+        assert cqe[i].user_data == i+1
+    # <<< difference END
+
+    assert cq_ready == loop
+    liburing.io_uring_cq_advance(ring, cq_ready)  # free seen entries
+
+
+def test_for_each(ring, cqe):
+    loop = 300
+    for i in range(loop):
+        sqe = liburing.io_uring_get_sqe(ring)
+        sqe.user_data = i+1
+
+    assert (counter := liburing.io_uring_submit(ring)) == loop
+    liburing.io_uring_wait_cqe_nr(ring, cqe, counter)  # wait for all to be ready
+
+    # >>> difference START 
+    cq_ready = 0
+    for index in range(liburing.io_uring_for_each_cqe(ring, cqe)):
+        assert cqe[i].user_data == i+1
+        cq_ready += 1
+    # <<< difference END
+
+    assert cq_ready == loop
+    liburing.io_uring_cq_advance(ring, cq_ready)  # free seen entries
