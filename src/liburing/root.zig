fb0323e08cd7107bd0ac368ef189aac61af4e661
Ritesh <ritesh@stealthcentral.com>
2026-03-04 07:49:43 -0600

    root: bench test

--- src/liburing/root.zig
+++ src/liburing/root.zig
@@ -1,24 +1,23 @@
 //! Liburing
 const oz = @import("PyOZ");
-const enums = @import("enum.zig");
 const class = @import("class.zig");
-const uring = @import("uring.zig");
-const statx = @import("statx.zig");
-const socket = @import("socket.zig");
-const consts = @import("const.zig");
-const errors = @import("error.zig");
-const helper = @import("helper.zig");
+// const socket = @import("socket.zig");
+// const statx = @import("statx.zig");
 
-pub const Module = oz.module(.{
-    .name = "_liburing",
-    .doc = "Zig wrapper around C Liburing, which is a helper to setup and tear-down io_uring instances.",
-    .funcs = &(uring.Functions ++ helper.Functions ++ errors.Functions),
-    .classes = &(class.Classes ++ socket.Classes ++ statx.Classes),
-    .enums = &enums.Enums,
-    .consts = &(consts.Constants ++ statx.Constants),
+pub const Liburing = oz.module(.{
+    .name = "liburing",
+    .from = &.{
+        // oz.withSource(@import("enum.zig"), @embedFile("enum.zig")),
+        // oz.withSource(@import("const.zig"), @embedFile("const.zig")),
+        // oz.withSource(@import("error.zig"), @embedFile("error.zig")),
+        // oz.withSource(@import("statx.zig"), @embedFile("statx.zig")),
+        // oz.withSource(@import("helper.zig"), @embedFile("helper.zig")),
+
+        // comment/uncomment out bottom
+        oz.withSource(@import("class.zig"), @embedFile("class.zig")),
+    },
+    // comment/uncomment out bottom
+    // .classes = &(class.Classes ++ socket.Classes),
+
+    // .funcs = &.{},
 });
-
-// Module initialization function
-// pub export fn PyInit__liburing() ?*oz.PyObject {
-//     return Module.init();
-// }
